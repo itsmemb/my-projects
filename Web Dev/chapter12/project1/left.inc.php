78006668fe1cb05db7d764582446a0d0fcbfcfe2
Max <78446326+itsmemb@users.noreply.github.com>
2023-02-05 15:27:36 -0600

    Add files via upload

---
+++ Web Dev/chapter12/project1/left.inc.php
@@ -0,0 +1,25 @@
+<aside class="col-md-2">
+    <div class="panel panel-info">
+        <div class="panel-heading">Continents</div>
+        <ul class="list-group">
+            <?php 
+                foreach($continents as $c) {
+                    echo "<li class='list-group-item'><a href='#'>$c</a></li>";
+                }
+            ?>
+        </ul>
+    </div>
+    <!-- end continents panel -->
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Popular</div>
+        <ul class="list-group">
+            <?php
+                foreach($countries as $key => $value) {
+                    echo "<li class='list-group-item'>" . generateLink("photos.php?iso=$key",$value,null) . "</li>";
+                }
+            ?>
+        </ul>
+    </div>
+    <!-- end continents panel -->
+</aside>
